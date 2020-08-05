91a53c27334befdea52de21cf34f9e3dd61c11f6
tjz123 <2331169+tjz123@user.noreply.gitee.com>
2020-08-05 23:29:52 +0800

    null

---
+++ store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		hasLogin: false,
+		userName: "",
+		testVersion: false
+	},
+	mutations: {
+		login(state, userName) {
+			state.userName = userName;
+			state.hasLogin = true;
+		},
+		logout(state) {
+			state.userName = "";
+			state.hasLogin = false;
+			uni.removeStorage({
+				key:'haslogin'
+			})
+		},
+		setTestVersion(state,a) {
+			state.testVersion = a
+		},
+	}
+})
+
+export default store
